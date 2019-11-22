2d2db1a830e51d97cb82f9c0aaf1cea9424e12eb
Suzan Nakatudde <suzannakatudde@Suzans-MacBook-Pro.local>
2019-11-22 17:44:39 -0600

    solve bug

--- flask-server/main.py
+++ flask-server/main.py
@@ -5,7 +5,7 @@ app = Flask("__main__")
 
 @app.route("/")
 def my_index():
-    return render_template("index.html", token="Hello world")
+    return render_template("index.html", flask_token="Hello world")
 
 
 app.run(debug=True)
